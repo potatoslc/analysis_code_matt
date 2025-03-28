3ae5aa966404216608040f0e0c1ab7f0408bfce1
potatoslc <potatoslc@icloud.com>
2025-03-28 15:08:20 -0600

    Add files via upload

--- all_functions.py
+++ all_functions.py
@@ -13,10 +13,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import h5py
 import time
-
+import cantera as ct
 
 import re
+def derivative(x, y):
+    if len(x)!=len(y):
+        print("Input files have different size")
+        return
+    
 
+    dx = np.diff(x)
+    dy = np.diff(y)
+    dydx = [y/x if x!= 0 else 0 for (y,x) in zip(dy,dx) ]
+    #dydx[0:-1] = dy/dx
+    
+    dydx.append((y[-1] - y[-2])/(x[-1] - x[-2]))
+
+    return dydx
 def file_findall_complex(header,directory):
     file_seq = []
     file_match_patten = header+r"_(\d+)\.csv"
@@ -37,18 +50,11 @@ def file_findall_complex(header,directory):
     file_seq.sort(key=lambda x:x[0])
     return file_seq
 
-def file_findall_lite(header,directory):
-    #file_seq = []
-    file_list = os.listdir(directory)
-    uin_files =[x for x in file_list if header in x]
-    #total_files = len(uin_files)
-    sorted(uin_files)
-    return uin_files
 
-
-def get_info(paradata_filename,isosurface_filename):
+def get_v_gc(paradata_filename,isosurface_filename):
     paradata_df = pd.read_csv(paradata_filename)
     isosurface_df = pd.read_csv(isosurface_filename)
+    current_time = paradata_df["Time"][0]
     """
     get u info from isosurface file
     x: Points:0
@@ -85,11 +91,41 @@ def get_info(paradata_filename,isosurface_filename):
     dz = float(paradata_df["Points:1"][min_diff_loc])
     g = dv/dz
 
+    return [g, x_flame_minx,y_flame_minx,x_minx,y_minx,current_time]
 
-    return [g, x_flame_minx,y_flame_minx,x_minx,y_minx]
 
-#paradata_df[paradata_df["x_filter"]>0]
-#
+def build_flame(T,P,phi,fuel,vel):
+    loglevel = 1
+    g = ct.Solution('mechanism.yaml')
+    mdotR = vel*g.density
+    ct.transport_model = 'multicomponent'
+    air = "O2:0.209,N2:0.791" # mol fraction of air with only N2,O2
+    g.set_equivalence_ratio(phi=phi, fuel="H2:1", oxidizer=air)
+	#g.X = {'H2':1, 'O2':.5/phi, 'N2':(.5*3.76)/phi}
+    g.TP = T,P
+    gridd = np.linspace(0,.01,1024)
+    flame = ct.FreeFlame(gas = g,grid = gridd)
+    #https://cantera.org/2.0/doxygen/html/classCantera_1_1FreeFlame.html
+    flame.transport_model = 'multicomponent'
+    flame.inlet.mdot = mdotR
+    flame.set_refine_criteria(ratio=4, slope=0.05, curve=0.1, prune=0.01)
+    flame.solve(loglevel=loglevel, refine_grid = False, auto = False)
+
+    return flame
+
+
+def get_flame_info(flame):
+    try:
+        flame_grid = flame.grid
+        speed = flame.velocity
+        
+        
+    except:
+        print("flame has erros")
+        return 
+
+    
+    return derivative(flame_grid, speed)
 
 
 
